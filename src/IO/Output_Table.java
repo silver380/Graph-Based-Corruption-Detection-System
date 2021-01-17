1da945f127ec51946b60d4063e41c4297dc863d0
amir1283212 <amir1283212@gmail.com>
2021-01-17 22:58:38 +0330

    writing file - bug fix

--- src/IO/Output_Table.java
+++ src/IO/Output_Table.java
@@ -7,6 +7,9 @@ import Graph.Algortihms.*;
 import IO.Search.Trie;
 import Graph.Vertex.*;
 import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import java.awt.*;
 import Graph.*;
@@ -21,14 +24,17 @@ public class Output_Table {
     private static Graph graph = new Graph();
     private static JButton jButton;
     private static boolean draw;
+    private static JTable jTable;
 
     public static void show(Graph g, boolean drawGraph) {
         draw = drawGraph;
         frame.getContentPane().removeAll();
         graph = g;
-        for(Vertex v : graph.getHashMap().values()) {
-            trie.add(v.key);
-        }
+        if (phase == 1)
+            for(Vertex v : graph.getHashMap().values()) {
+                trie.add(v.key);
+            }
+
         jButton = new JButton("Phase: " + phase + "." + (phase == 4 ? "" : " Click to continue."));
         if (phase != 4)
             jButton.addActionListener(actionListener);
@@ -36,10 +42,20 @@ public class Output_Table {
         textField.setSize(300, 40);
         textField.getDocument().addDocumentListener(listener);
 
-        frame.add(textField, BorderLayout.NORTH);
-        frame.add(jButton, BorderLayout.SOUTH);
-       // System.out.println("we are here");
-        draw("");
+        System.out.println("we are here");
+
+        if (phase == 1) {
+            draw("");
+        }
+        else if (phase == 2) {
+            draw("");
+        }
+        else if (phase == 3) {
+            draw("sus3");
+        }
+        else {
+            draw("sus4");
+        }
 
         if (phase == 1) {
             drawTheGraph();
@@ -48,19 +64,38 @@ public class Output_Table {
             System.out.println("Phase 2 is running...");
             PhaseTwo.susFinder(graph);
             System.out.println("Phase 2 result is ready");
+            write(1);
             drawTheGraph();
         }
         else if (phase == 3) {
             System.out.println("Phase 3 is running...");
             PhaseThree.secondCheck(graph);
             System.out.println("Phase 3 result is ready");
+            write(2);
             drawTheGraph();
         }
         else if (phase == 4) {
             System.out.println("Phase 4 is running...");
             PhaseFour.thirdCheck(graph);
             System.out.println("Phase 4 result is ready");
+            write(3);
             drawTheGraph();
+        }
+    }
+
+    private static void write(int s) {
+        try {
+            File file = new File("suspect" + (s + 1) + " .txt");
+            FileWriter myWriter = new FileWriter(file);
+            for (Vertex vertex : graph.getHashMap().values()) {
+                if (vertex instanceof People && ((People) vertex).isSuspect == s) {
+                    myWriter.write(vertex.toString() + "\n");
+                }
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 
@@ -75,7 +110,16 @@ public class Output_Table {
     };
 
     private static void draw(String search) {
+        frame.getContentPane().removeAll();
+        frame.getContentPane().revalidate();
+        jTable = null;
+
+        System.gc();
+        System.out.println("removed");
+        System.out.println(Arrays.toString(frame.getComponents()));
         String[][] data;
+
+//        textField.setText(search + "");
 
         if (search.equals("sus2") || search.equals("sus3") || search.equals("sus4")) {
             ArrayList<String> ids = new ArrayList<>();
@@ -107,8 +151,9 @@ public class Output_Table {
                 data[i][2] = graph.getHashMap().get(list.get(i)).toString();
             }
         }
-        //System.out.println("the jtable is playing with us");
-        JTable jTable = new JTable(data, columns);
+        System.out.println("the jtable is playing with us");
+
+        jTable = new JTable(data, columns);
         jTable.getColumnModel().getColumn(0).setMinWidth(50);
         jTable.getColumnModel().getColumn(1).setMinWidth(80);
         jTable.getColumnModel().getColumn(2).setMinWidth(500);
@@ -116,22 +161,23 @@ public class Output_Table {
         JScrollPane scrollPane = new JScrollPane(jTable);
 
         frame.add(scrollPane);
+        frame.add(textField, BorderLayout.NORTH);
+        frame.add(jButton, BorderLayout.SOUTH);
         frame.pack();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setMinimumSize(new Dimension(630, 400));
         frame.setVisible(true);
+        data = null;
     }
 
     static DocumentListener listener = new DocumentListener() {
         @Override
         public void insertUpdate(DocumentEvent e) {
-            frame.getContentPane().remove(2);
             draw(textField.getText());
         }
 
         @Override
         public void removeUpdate(DocumentEvent e) {
-            frame.getContentPane().remove(2);
             draw(textField.getText());
         }
 
@@ -141,3 +187,4 @@ public class Output_Table {
         }
     };
 }
+
